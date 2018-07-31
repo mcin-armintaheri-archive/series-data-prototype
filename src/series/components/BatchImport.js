f1703567d0e94841de765dc7d464e65c518ab077
DianaSerra <dianaiserra@hotmail.com>
2018-07-31 14:37:35 -0400

    Batch Import with custom button for file selection

--- src/series/components/BatchImport.js
+++ src/series/components/BatchImport.js
@@ -3,10 +3,29 @@ import {Button, Row, Col} from "react-bootstrap";
 import * as R from "ramda";
 
 export default class BatchImport extends Component {
+  
   render(){
     const {heading, batches, setBatches} = this.props;
     const addBatch=()=> R.append({files: []}, batches);
-    
+    const addFileToBatch=(index, file) =>{
+      const { batches, setBatches } = this.props;
+      const innerArrayLens = R.lensPath([index, "files"]);
+      setBatches( R.over(innerArrayLens, R.unless(R.isNil, R.append(file)), batches ));
+    };
+    const askForFile=(index) =>{
+      const input = document.createElement("input");
+      input.type = "file";
+      input.accept = ".csv";
+      input.multiple = true;
+      input.click();
+      input.addEventListener("change", function(event) {
+          for(var i=0; i<event.target.files.length; i++){
+            addFileToBatch(index, event.target.files[i]);
+          }
+          
+        }
+      );
+    };
     
     return(
       <div>    
@@ -19,12 +38,12 @@ export default class BatchImport extends Component {
                 <label>Batch {index+1} :</label>
               </Col>
               <Col>
-                <input type='file' accept=".csv" multiple/>
+                <Button bsSize="small" onClick= {()=>askForFile(index)}>Upload Table</Button>
               </Col>
             </Row> <br/>
               <ul>
                 {batch.files.map((file) =>
-                  <li>{file}</li>
+                  <li>{file.name}</li>
                 )}
               </ul>
           </div>
@@ -32,21 +51,21 @@ export default class BatchImport extends Component {
       </div>
     )
   };
+  
 }
 
 export class BatchImportTest extends Component{
   constructor(props){
-      super(props);
-      this.state= {
-          heading: "Series Metadata",
-          batches:[
-            {files: ['hello.csv', 'another.csv']}
-          ]
-        }
-      };
+    super(props);
+    this.state= {
+      heading: "Series Metadata",
+      batches:[
+      ]
+    };
+  };
   
   render(){
     const setBatches= batches => this.setState({batches});
-    return <BatchImport heading= {this.state.heading} batches= {this.state.batches} setBatches={setBatches}/>
+    return <BatchImport heading= {this.state.heading} batches= {this.state.batches} setBatches={setBatches} />
   }
 }
